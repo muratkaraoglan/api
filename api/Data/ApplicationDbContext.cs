750425cf723eb7dfd0bab451142fa9b79ba6da0d
Murat Karaoglan <m.karaoglan1604@gmail.com>
2026-07-13 01:45:00 +0300

    EF Core

---
+++ api/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api.Data
+{
+    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    {
+        public DbSet<Stock> Stock { get; set; }
+        public DbSet<Comment> Comment { get; set; }
+
+    }
+}
--- api/Program.cs
+++ api/Program.cs
@@ -1,3 +1,5 @@
+using api.Data;
+using Microsoft.EntityFrameworkCore;
 using Scalar.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -7,6 +9,11 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
